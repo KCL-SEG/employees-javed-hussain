25d4446b010a4f5b1dc8ddd2fd7a20513c098b0c
javed-hussain <j.hussain1204@gmail.com>
2023-12-02 14:52:26 +0000

    e

--- employee.py
+++ employee.py
@@ -2,15 +2,14 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 class Employee:
-    def __init__(self, name, contractType, commissionType, wage):
+    def __init__(self, name, contractLength, commissionType, wage):
         self.name = name
         self.contractType = contractType
         self.comission = commissionType
         self.wage = wage
 
     def get_pay(self):
-        if self.contractType == "monthly":
-            pay = self.wage
+        pay = self.wage * self.contractLength
         return pay
 
     def __str__(self):
@@ -18,19 +17,19 @@ class Employee:
 
 
 # Billie works on a monthly salary of 4000.  Their total pay is 4000.
-billie = Employee('Billie', 'monthly', 'None', 4000)
+billie = Employee('Billie',1, 'None', 4000)
 
 # Charlie works on a contract of 100 hours at 25/hour.  Their total pay is 2500.
-charlie = Employee('Charlie', "", "", 25)
+charlie = Employee('Charlie',100, "", 25)
 
 # Renee works on a monthly salary of 3000 and receives a commission for 4 contract(s) at 200/contract.  Their total pay is 3800.
-renee = Employee('Renee',"","",4)
+renee = Employee('Renee',1,"",3000)
 
 # Jan works on a contract of 150 hours at 25/hour and receives a commission for 3 contract(s) at 220/contract.  Their total pay is 4410.
-jan = Employee('Jan',"","",5)
+jan = Employee('Jan',150,"",25)
 
 # Robbie works on a monthly salary of 2000 and receives a bonus commission of 1500.  Their total pay is 3500.
-robbie = Employee('Robbie',"","",5)
+robbie = Employee('Robbie',1,"",2000)
 
 # Ariel works on a contract of 120 hours at 30/hour and receives a bonus commission of 600.  Their total pay is 4200.
-ariel = Employee('Ariel',"","",5)
+ariel = Employee('Ariel',120,"",30)
